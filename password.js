f9543fba47113b4bc3eaa2ae65e42ff56ae8a16d
cynygejm <asiaglina@o2.pl>
2017-11-09 17:16:39 +0100

    sam html

---
+++ password.js
@@ -0,0 +1,9 @@
+createElements();
+/*<div id= "container">
+        <div id="logo">"Password generator"</div>
+        <div id="content">
+            <label for="length">"Give me password length: "</label>
+        <input type="text" id="length">
+            <button type="button" >"OK"</button>
+        </div>
+        </div>*/
